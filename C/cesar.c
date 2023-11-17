624bf40a5e0a4c0945f945351f512a16d84b248f
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-11-17 16:30:13 +0100

    vie2

--- C/cesar.c
+++ C/cesar.c
@@ -53,6 +53,17 @@ void sumarK(int *numeros, int n, int k, int lenAlfabeto){
     }
 }
 
+void restarK(int *numeros, int n, int k, int lenAlfabeto){
+	int i;
+	
+	for (i=0; i<n; i++){
+		numeros[i] = (numeros[i] - k);
+		if (numeros[i]<0){
+			numeros[i] += lenAlfabeto;
+		}
+	}
+}
+
 void decodificar(const int *numeros, const char *alfabeto, char *mensaje_enc, int n){
     int i;
     char c;
@@ -61,26 +72,57 @@ void decodificar(const int *numeros, const char *alfabeto, char *mensaje_enc, in
         c = alfabeto[numeros[i]];
         mensaje_enc[i] = c;
     }
+    mensaje_enc[i] = '\0';
 }
 
-int main(){
-    char mensaje[TAM_MEN] = {"mi tno es 567889922"}, mensaje_enc[TAM_MEN];
+void encriptar(const char *mensaje, const char *alfabeto, int n, char *mensaje_enc, int k){
     int numeros[TAM_MEN];
-    char alfabeto[ALF * 2 + 10 + 1 + 1]; // dos alfabetos + 10 digitos + 1 espacio + 1 \0
-
-    int n = strlen(mensaje);
-    
-    cargarAlfabeto(alfabeto);
-    puts(alfabeto);
 
     codificar(mensaje, alfabeto, numeros, n);
-    puts(mensaje);
-    imprimir(numeros, n);
+    //puts(mensaje);
+    //imprimir(numeros, n);
 
     // Sumar k a cada número:
-    sumarK(numeros, n, K, strlen(alfabeto));
+    sumarK(numeros, n, k, strlen(alfabeto));
     imprimir(numeros, n);
 
     decodificar(numeros, alfabeto, mensaje_enc, n);
+    //puts(mensaje_enc);
+}
+
+void desencriptar(const char *mensaje_enc, const char *alfabeto, char *mensaje2, int n, int k){
+    int numeros[TAM_MEN];
+
+    codificar(mensaje_enc, alfabeto, numeros, n);
+
+    // restar k al array de numeros: si el número < 0 sumar la longitud del alf.
+    restarK(numeros, n, k, strlen(alfabeto));
+    imprimir(numeros, n);
+
+    // decodificar mensaje_enc
+    decodificar(numeros, alfabeto, mensaje2, n);
+}
+
+int main(){
+    char mensaje[TAM_MEN] = {""}, mensaje_enc[TAM_MEN], mensaje2[TAM_MEN];    
+    char alfabeto[ALF * 2 + 10 + 1 + 1]; // dos alfabetos + 10 digitos + 1 espacio + 1 \0
+    int n;
+    
+    cargarAlfabeto(alfabeto);
+    puts(alfabeto);
+    printf("num letras alf: %ld\n", strlen(alfabeto));
+
+    puts("Teclear mensaje: ");
+    fgets(mensaje, TAM_MEN, stdin);
+    if (strlen(mensaje) < TAM_MEN){        
+        mensaje[strlen(mensaje)-1]='\0';
+        puts(mensaje);
+    }
+    n = strlen(mensaje);
+   
+    encriptar(mensaje, alfabeto, n, mensaje_enc, K);
     puts(mensaje_enc);
+
+    desencriptar(mensaje_enc, alfabeto, mensaje2, n, K);
+    puts(mensaje2);
 }
