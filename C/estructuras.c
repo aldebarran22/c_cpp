024e167016a901993fb42cfa8cb2a11ca5f09eae
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-11-17 17:42:54 +0100

    vier4448

--- C/estructuras.c
+++ C/estructuras.c
@@ -20,8 +20,30 @@ typedef struct {
 } THora; // El nombre de un tipo nuevo
 
 void imprimirFecha(struct TFecha f){
-    printf("%02d/%02d/%02d", f.dd, f.mm, f.yy);
+    printf("%02d/%02d/%d", f.dd, f.mm, f.yy);
 }
+
+void imprimirHora(THora hora){
+    printf("%02d:%02d:%02d", hora.h, hora.m, hora.s);
+}
+
+void leerHora(THora *hora){
+    //(*hora).h
+    char aux[5];
+    
+    printf("HH:");
+    fgets(aux, 5, stdin);
+    hora->h = atoi(aux);
+
+    printf("MM:");
+    fgets(aux, 5, stdin);
+    hora->m = atoi(aux);
+
+    printf("SS:");
+    fgets(aux, 5, stdin);
+    hora->s = atoi(aux);
+}
+
 
 struct TFecha fechaRand(){
     int dias[] = {31,28,31,30,31,30,31,31,30,31,30,31};
@@ -34,6 +56,15 @@ struct TFecha fechaRand(){
     return f;
 }
 
+THora horaRand(){
+    THora hora;
+
+    hora.h = rand() % 24;
+    hora.m = rand() % 60;
+    hora.s = rand() % 60;
+
+    return hora;
+}
 
 int main(){
     THora ahora = {16, 42, 56};
@@ -47,9 +78,16 @@ int main(){
 
     for (i = 0 ; i < 20 ; i++){
         hoy = fechaRand();
-        imprimirFecha(hoy);
+        imprimirFecha(hoy);        
+        puts("");
+        ahora = horaRand();
+        imprimirHora(ahora);
         puts("");
     }
+
+    // Leer una hora:
+    leerHora(&ahora);
+    imprimirHora(ahora);
 
     return 0;
 }
