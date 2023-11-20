55f882b86537b15773175703c1a95b2bad3a453a
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-11-20 17:50:41 +0100

    lun2345

--- C/ficheros_estructuras/ficheros.c
+++ C/ficheros_estructuras/ficheros.c
@@ -1,12 +1,51 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "estructuras.h"
 #include "funciones.h"
 
+#define N_STRUCT 25
+
+void grabarFichero(const char *path){
+    FILE *f;
+    int i;
+    TUser user;
+
+    if ((f=fopen(path, "ab"))!=NULL){
+        for (i = 0 ; i < N_STRUCT ; i++){
+            user.fecha = fechaRand();
+            user.hora = horaRand();
+            usuarioRand(user.usuario);
+
+            fwrite(&user, sizeof(TUser), 1, f);
+        }
+        fclose(f);
+    }
+}
+
+void leerFichero(const char *path){
+    FILE *f;    
+    TUser user;
+
+    if ((f=fopen(path, "rb"))!=NULL){
+        while (fread(&user, sizeof(TUser), 1, f)){
+            imprimirFecha(user.fecha);
+            printf("\t");
+            imprimirHora(user.hora);
+            printf("\t");
+            puts(user.usuario);
+        }
+        fclose(f);
+    }
+}
+
 
 int main(){
-    
+    srand(time(0));
+    grabarFichero("./estructuras2.bin");
+    leerFichero("./estructuras2.bin");
     return 0;
 }
