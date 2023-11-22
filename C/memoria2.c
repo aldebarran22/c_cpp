754fc1aa643bfb8fdb7365179f01cde8017ec26e
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-11-22 16:27:00 +0100

    mie1

--- C/memoria2.c
+++ C/memoria2.c
@@ -43,17 +43,51 @@ void liberar(int **p){
     *p = NULL;
 }
 
+void funcion1(int n){
+    n++;
+    printf("\n en funcion1 n = %d", n);
+}
+
+void funcion2(int *n){
+    printf("\n en en funcion dir: %p", n);
+    *n = 100;
+}
+
+void funcion3(int *ptr){
+    ptr = (int *) malloc(3 * sizeof(int));
+    printf("\nptr: %d %d %d", ptr[0], ptr[1], ptr[2]);
+    funcion2(ptr);
+    printf("\nptr: %d %d %d", ptr[0], ptr[1], ptr[2]);
+    printf("\n en funcion3 ptr: %p", ptr);
+}
+
 int main(){
     int *p = NULL;
+    int array[] = {1,2,3,4,5};
+    int n=0;
+
+    printf("\n n = %d", n);
+    funcion1(n);
+    printf("\n n = %d", n);
+
+    funcion2(&n);
+    printf("\n n = %d", n);
+    printf("\nArray: %d %d %d", array[0], array[1], array[2]);
+    printf("\n en main array: %p", array);
+    funcion2(array);
+    printf("\nArray: %d %d %d", array[0], array[1], array[2]);
+    funcion3(p);
+    printf("\n en main p: %p", p);
    
     //reservar(&p);
+    /*
     p = reservar2();
     printf("\ndir en main: %p\n", p);
     imprimir(p);
     mul2(p);
     liberar(&p);  
     printf("\ndir en main despues de free: %p\n", p);
-
+    */
     puts("");
     return 0;
 }
